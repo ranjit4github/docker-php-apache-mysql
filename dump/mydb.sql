62c7895327237832c5e8820cfa48e7469d6b0b0b
Doan Quang Huy <quanghuybest@gmail.com>
2024-03-10 22:00:15 +0700

    refactor: change directory structure

--- dump/mydb.sql
+++ dump/mydb.sql
@@ -2,13 +2,13 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 CREATE TABLE `Person` (
-  `id` int(11) NOT NULL,
+  `id` INT(11) AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 INSERT INTO `Person` (`id`, `name`) VALUES
-(1, 'Ranjit'),
-(2, 'Rakesh'),
-(3, 'Dinesh'),
-(4, 'Karthik');
+(1, 'Đoàn Quang Huy'),
+(2, 'Nguyễn Văn Tràm'),
+(3, 'Trần Ngọc Hân'),
+(4, 'Nguyễn Thúy Liên');
